03f59958b63aabe315ff1aa88643da5750b1201a
Juraj Kirchheim <back2dos@googlemail.com>
2017-12-18 11:29:12 +0100

    Slightly expand Value API.

--- src/coconut/data/Value.hx
+++ src/coconut/data/Value.hx
@@ -8,9 +8,13 @@ using haxe.macro.Tools;
 using tink.MacroApi;
 #end
 
+@:forward
 abstract Value<T>(Observable<T>) from Observable<T> to Observable<T> {
+  public inline function or(constant:T):Observable<T>
+    return if (this == null) constant else this;
+
   @:from macro static function lift(e) {
-    //TODO: be a bit smarter about detecting constants
+    //TODO: be a bit smarter about detecting constants and also make sure literal `null` is handled properly
     return
       switch typeExpr(e) {
         case { expr: TConst(_) }:
@@ -22,12 +26,12 @@ abstract Value<T>(Observable<T>) from Observable<T> to Observable<T> {
               function get<T>(o:tink.state.Observable<T>) return o.value;
               get($e);
             }).toComplex();
-            switch getExpectedType() {
+            switch getExpectedType().reduce() {
               case TAbstract(_.get().module => 'coconut.data.Value', [_.toComplex() => expected]):
                 typeof(macro @:pos(e.pos) ((cast null : $found) : $expected));
                 macro @:pos(e.pos) ($e : tink.state.Observable<$found>).map(function (x):$expected return x);
-              default: 
-                throw 'assert';
+              case v: 
+                throw 'assert: $v';
             }
           }
             // e.reject('${type.toString()} should be ${getExpectedType().toString()}')
